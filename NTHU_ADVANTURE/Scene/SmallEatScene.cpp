685f7f7b5c78e1230fd15875aaac1284df6d9d67
tingenchen <tingen168@gmail.com>
2025-06-13 01:09:35 +0800

    l

--- NTHU_ADVANTURE/Scene/SmallEatScene.cpp
+++ NTHU_ADVANTURE/Scene/SmallEatScene.cpp
@@ -166,7 +166,7 @@ void SmallEatScene::Initialize() {
 
     LabelGroup->AddNewObject(moneyLabel = new Engine::Label(std::to_string(LogScene::money), "title.ttf", 48, 130, 70, 255, 255, 255, 255, 0.5, 0.5));
     LabelGroup->AddNewObject(moneyImage = new Engine::Image("play/dollar.png", 20, 35, 56, 56));
-    if (LogScene::haveAxe) AddNewObject(axeImage = new Engine::Image("stage-select/axe.png", 20, 105, 56, 56));
+    if (LogScene::haveAxe) LabelGroup->AddNewObject(axeImage = new Engine::Image("stage-select/axe.png", 20, 105, 56, 56));
 }
 
 void SmallEatScene::Terminate() {
